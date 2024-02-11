46cd606f2d605b1c24e19dfbe64d2a6755d95c58
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-11 14:08:34 +0900

    UI作成

---
+++ FaceBook/TimeLineTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TimeLineTableViewCell.swift
+//  FaceBook
+//
+//  Created by 澤木柊斗 on 2024/02/11.
+//
+
+import UIKit
+
+class TimeLineTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
