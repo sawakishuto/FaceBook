ba1123857b6a58346d9bc3f03a1096f073faf1e0
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-11 14:08:49 +0900

    viewController変更

--- FaceBook/ViewController.swift
+++ FaceBook/ViewController.swift
@@ -7,8 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return timeLines.count + 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath)
+            return cell
+        default:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TimeLineCell", for: indexPath) as! TimeLineTableViewCell
+            return cell
 
+        }
+    }
+
+    private var timeLines: [Timeline] = Timelines.createTimelines() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
