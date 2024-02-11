fc940e612b175b6107a43413398a00b5b1ea4407
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-11 14:09:14 +0900

    TableViewController作成

--- FaceBook/TimeLineTableViewCell.swift
+++ FaceBook/TimeLineTableViewCell.swift
@@ -9,15 +9,25 @@ import UIKit
 
 class TimeLineTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var bodyTextView: UITextView!
+    @IBOutlet weak var CommentCountLabel: UILabel!
+    @IBOutlet weak var LikedCountLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var commentButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
+    var timeLine: Timeline! {
+        didSet {
+            profileImageView.image = UIImage(named: timeLine.user.imageName)
+            userNameLabel.text = timeLine.user.name
+            bodyTextView.text = timeLine.body
+            if timeLine.isLiked {
+                LikedCountLabel.text = "You and \(timeLine.likeCount - 1) others liked"
+            } else {
+                LikedCountLabel.text = "\(timeLine.likeCount) people liked"
+            }
+            CommentCountLabel.text = "\(timeLine.commentCount) comment"
+        }
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
